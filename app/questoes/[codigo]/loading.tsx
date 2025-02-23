d0f8d60e6ee5fb05dd2159839b1eaa84e37ae9e6
Bruno Sousa <bbarros.sousa@hotmail.com>
2025-02-22 21:27:16 -0300

    adding loading.tsx

---
+++ app/questoes/[codigo]/loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="w-[90%] flex mt-[100px] ml-auto mb-[100px] mr-auto mb-3 flex w-full">
+      <Skeleton className=" h-20 w-20 rounded-full mr-5 ml-5" />
+      <div className="space-y-2 w-full ">
+        <Skeleton className="h-7 mt-3 min-w-[250px] mr-10" />
+        <Skeleton className="h-7 min-w-[200px] mr-10" />
+      </div>
+    </div>
+  );
+}
